9c10453f0a69ecbfb1296248795386eb328260a1
jenifersambul <63536411+jenifersambul@users.noreply.github.com>
2021-12-02 02:10:54 +0800

    Update About.jsx

--- FinalProjectFWD/src/About.jsx
+++ FinalProjectFWD/src/About.jsx
@@ -8,6 +8,8 @@ function About() {
     
     <Button color='secondary' className='ml-2'><Link exact to="/About">About</Link></Button>
   <h1>About</h1>
+        <p>Aplikasi ini merupakan aplikasi ReactJS pencarian buku yang diambil dari Google Books Search API.</p>
+        <p>Dalam aplikasi ini user bisa mencari buku berdasarkan judul dan dapat melihat penjelasan atau sinopsis buku.</p>
         <p>Jenifer Putri Sambul (105011810074), Sistem Informasi</p>
   </>
   )
